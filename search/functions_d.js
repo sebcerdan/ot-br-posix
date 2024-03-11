a6ead8c8ee6eb92f97a17dad4e00dba24dfed3ad
jwhui <jwhui@users.noreply.github.com>
2024-03-11 21:53:59 +0000

    deploy: c88a5fce6659b0e27280df7ef2d03072452c63ec

--- search/functions_d.js
+++ search/functions_d.js
@@ -8,11 +8,11 @@ var searchData=
   ['otbrerrorstring_1307',['otbrErrorString',['../d0/da9/logging_8hpp.html#ada4d1801b5705ba2b957fd1802b8a984',1,'logging.cpp']]],
   ['otbrlog_1308',['otbrLog',['../d0/da9/logging_8hpp.html#aae81f9dc34535f2621fe586172f968e6',1,'logging.cpp']]],
   ['otbrlogdeinit_1309',['otbrLogDeinit',['../d0/da9/logging_8hpp.html#a5a2bf385c625fa5e9beaba21d26326fe',1,'logging.cpp']]],
-  ['otbrlogenablesyslog_1310',['otbrLogEnableSyslog',['../d0/da9/logging_8hpp.html#a762b4410fc095825139d18166e8a64e6',1,'logging.hpp']]],
-  ['otbrloggetdefaultlevel_1311',['otbrLogGetDefaultLevel',['../d0/da9/logging_8hpp.html#a5b31d571fd35078bf0d4c1fe252f1e8b',1,'logging.cpp']]],
-  ['otbrloggetlevel_1312',['otbrLogGetLevel',['../d0/da9/logging_8hpp.html#afc9ce29e790e067ca79760c96f794d43',1,'logging.cpp']]],
-  ['otbrloginit_1313',['otbrLogInit',['../d0/da9/logging_8hpp.html#a616821845b17b9d0b9781f7beb7f0716',1,'logging.cpp']]],
-  ['otbrlogsetlevel_1314',['otbrLogSetLevel',['../d0/da9/logging_8hpp.html#aa4b0bf2713442aa78a8c2ecf5c5f8c5d',1,'logging.cpp']]],
+  ['otbrloggetdefaultlevel_1310',['otbrLogGetDefaultLevel',['../d0/da9/logging_8hpp.html#a5b31d571fd35078bf0d4c1fe252f1e8b',1,'logging.cpp']]],
+  ['otbrloggetlevel_1311',['otbrLogGetLevel',['../d0/da9/logging_8hpp.html#afc9ce29e790e067ca79760c96f794d43',1,'logging.cpp']]],
+  ['otbrloginit_1312',['otbrLogInit',['../d0/da9/logging_8hpp.html#aaf513c85cc7d7cf211d8d9eb696aaf3f',1,'logging.cpp']]],
+  ['otbrlogsetlevel_1313',['otbrLogSetLevel',['../d0/da9/logging_8hpp.html#aa4b0bf2713442aa78a8c2ecf5c5f8c5d',1,'logging.cpp']]],
+  ['otbrlogsyslogsetenabled_1314',['otbrLogSyslogSetEnabled',['../d0/da9/logging_8hpp.html#af740e6936276863cd271888f9d24e088',1,'logging.cpp']]],
   ['otbrlogv_1315',['otbrLogv',['../d0/da9/logging_8hpp.html#aaf2c504d00faeea0f5e4a8e45ed7a871',1,'logging.cpp']]],
   ['otbrlogvnofilter_1316',['otbrLogvNoFilter',['../d0/da9/logging_8hpp.html#a13b760a72ea9fe45589aa46998847373',1,'logging.cpp']]]
 ];
